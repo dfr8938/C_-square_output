feef7d34e64755c64d0110fe35b5424eae449565
Marth Strong <mastrong@ev-g4.kzn.21-school.ru>
2020-08-09 20:05:04 +0300

    first commit

---
+++ rush03.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void ft_putchar(char n);
+
+void	rush(int x, int y)
+{
+	char arr[x][y];
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;	
+	while (j < y)
+	{
+		while (i < x)
+		{
+			if ((i == 0 && j == 0) || (i == 0 && j == y - 1))
+				arr[x][y] = 'A';
+			else if ((i == x - 1 && j == 0) || (i == x - 1 && j == y - 1))
+				arr[x][y] = 'C';
+			else if ((j != 0 && j != y - 1) && (i != 0 && i != x - 1))
+				arr[x][y] = ' ';
+			else
+				arr[x][y] = 'B';
+			ft_putchar(arr[x][y]);
+			i++;
+		}
+		if (i == x - 1 && i == 0)
+			ft_putchar(arr[x][y]);
+			write(1, "\n", 1);
+		i = 0;
+		j++;
+	}
+}
+
+int main()
+{
+	rush(20, 20);
+	return 0;
+}
